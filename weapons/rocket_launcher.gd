a6ad59a723499e94e0dfba4a5f4c702aba054b0b
Saebyul Hong <104213415+StarshineStudios@users.noreply.github.com>
2026-04-15 16:27:51 -0400

    fix rocket

--- weapons/rocket_launcher.gd
+++ weapons/rocket_launcher.gd
@@ -1,44 +1,31 @@
 extends Gun
+class_name RocketLauncher
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
 func shoot(player: Player):
 	if is_reloading:
 		return
-	
 	if mag <= 0:
 		start_reload()
 		return
-	
 	if not can_shoot:
 		return
-	$AnimationPlayer.play("shoot")
-	
+
 	can_shoot = false
 	mag -= 1
 	ammo_changed.emit(mag, mag_size)
 	shot_fired.emit()
 	muzzle_flash.emitting = true
-	
+	$AnimationPlayer.play("shoot")
 	get_tree().create_timer(fire_rate).timeout.connect(func(): can_shoot = true)
-	
+
 	if player.is_multiplayer_authority():
 		_request_rocket.rpc_id(1,
-		player.get_multiplayer_authority(),  # ← fix here
-		$Muzzle.global_transform
+			player.get_multiplayer_authority(),
+			$Muzzle.global_transform
 		)
 
 @rpc("any_peer", "call_local", "reliable")
 func _request_rocket(shooter_id: int, spawn_transform: Transform3D):
 	if not multiplayer.is_server():
 		return
-	# game node does the actual spawn
 	get_tree().get_root().get_node("Game").spawn_rocket(shooter_id, spawn_transform)
